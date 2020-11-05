b57be5b6f35fcd79abfb905d659da56560d40110
Pinny Markowitz <pmarkowitz@teladoc.com>
2020-11-05 10:43:21 -0500

    bump version for bug fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zmanim",
-    version="0.3.0",
+    version="0.3.1",
     author="Pinny Markowitz",
     author_email="pinny@mwitz.com",
     description="A Zmanim library for Python",
